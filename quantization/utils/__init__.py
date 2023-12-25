f712430a156d45cdecc8e0fdca61869ffd089fc4
fafu <fa.fu@casivision.com>
2023-12-25 16:33:12 +0800

    loss

--- quantization/utils/__init__.py
+++ quantization/utils/__init__.py
@@ -1,3 +1,4 @@
 from .my_logging import init_logging
 from .dataset import segDataset
 from .model import unet,wrapUnet
+from .criterion import cross_entropy2d, bootstrapped_cross_entropy2d, DiceLoss, CriterionAll
