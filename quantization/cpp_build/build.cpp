5b6e7254176247babb07443f1c5ce22c01e66fe1
fafu <fa.fu@casivision.com>
2023-12-29 10:33:55 +0800

    增加INT8量化build

--- quantization/cpp_build/build.cpp
+++ quantization/cpp_build/build.cpp
@@ -4,18 +4,21 @@
 * @date: 2023/12/28 9:34
 * @description:
 ***************************************************/
-
-#include <NvInferVersion.h>
-#include <NvInfer.h>
-#include <NvOnnxParser.h>
 #include <iostream>
 #include <fstream>
 #include <memory>
 #include <thread>
 #include <string>
 
+#include <opencv2/opencv.hpp>
+#include <NvInferVersion.h>
+#include <NvInfer.h>
+#include <NvOnnxParser.h>
+
+
 using namespace std;
 using namespace nvinfer1;
+using namespace cv;
 
 class NVLogger: public nvinfer1::ILogger {
 public:
@@ -32,10 +35,123 @@ public:
     }
 };
 
+class CNNCalibrator : public IInt8EntropyCalibrator2 {
+    public:
+        CNNCalibrator(const string &calibrationDataFolder, const string &imageSuffix,int calibrationNum) noexcept(false);
+        ~CNNCalibrator() noexcept override;
+
+    public:
+
+        int32_t getBatchSize() const noexcept override ;
+        bool getBatch(void *bindings[], char const *names[], int32_t nbBindings) noexcept override;
+        void const *readCalibrationCache(std::size_t &length) noexcept override;
+        void writeCalibrationCache(void const *ptr, std::size_t length) noexcept override;
+
+    private:
+        vector<string> m_vImagesPath;               // 矫正数据
+
+        int m_iCalibrationNum{0};                   // 矫正总次数
+        int m_iCalibrationNumTimes{0};              // 现在是第几次矫正
+
+        float* m_pBufferD{nullptr};                 // 输入在显存上的空间地址
+        string m_sCacheFile{"cache.cache"};           // 缓存文件路径
+    };
+
+CNNCalibrator::CNNCalibrator(const string &calibrationDataFolder, const string &imageSuffix, int calibrationNum):
+        m_iCalibrationNum(calibrationNum)
+{
+    // 获得所有图片路径
+    cv::glob(calibrationDataFolder + "*" + imageSuffix, m_vImagesPath);
+
+    // 分配显存
+    cudaMalloc((void**)&m_pBufferD, sizeof(float) * 1 * 1 * 256 * 256);
+
+}
+
+bool CNNCalibrator::getBatch(void* bindings[], char const* names[], int32_t nbBindings) noexcept
+{
+    if (m_iCalibrationNumTimes < m_iCalibrationNum)
+    {
+        int idx = m_iCalibrationNumTimes%m_vImagesPath.size();
+        Mat image = imread(m_vImagesPath[idx], 0);
+
+        cv::Mat convertToFP32;
+        image.convertTo(convertToFP32, CV_32F);
+        cudaMemcpy(m_pBufferD, convertToFP32.data, 256*256* sizeof(float), cudaMemcpyHostToDevice);
+
+        bindings[0] = m_pBufferD;
+        m_iCalibrationNumTimes++;
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+CNNCalibrator::~CNNCalibrator() noexcept
+{
+    if (m_pBufferD != nullptr)
+    {
+        cudaFree(m_pBufferD);
+    }
+}
+
+int32_t CNNCalibrator::getBatchSize() const noexcept
+{
+    return 1;
+}
+
+void const* CNNCalibrator::readCalibrationCache(std::size_t& length) noexcept
+{
+
+    std::fstream f;
+    f.open(m_sCacheFile, std::fstream::in);
+    if (f.fail())
+    {
+        return nullptr;
+    }
+    char* ptr = new char[length];
+    if (f.is_open())
+    {
+        f >> ptr;
+    }
+    return ptr;
+}
+
+void CNNCalibrator::writeCalibrationCache(void const* ptr, std::size_t length) noexcept
+{
+
+    std::ofstream f(m_sCacheFile, std::ios::binary);
+    if (f.fail())
+    {
+        return;
+    }
+    f.write(static_cast<char const*>(ptr), static_cast<long long>(length));
+    if (f.fail())
+    {
+        return;
+    }
+    f.close();
+}
+
 int main(int argc, char** argv){
     string src = "/root/data/fufa/modelcompression/quantization/checkpoints/wafer-train.onnx";
-    string dst = "/root/data/fufa/modelcompression/quantization/checkpoints/wafer-train-fp16.trt";
-    int mode = 1;   # 1 FP16; 2 INT8
+    string dst, calibrator_dataset_path, imageSuffix;
+    int calibrationNum;
+    int TYPE = 1;
+    int model = 1;
+    if(TYPE==1){
+        dst = "/root/data/fufa/modelcompression/quantization/checkpoints/wafer-train-fp16.trt";
+        mode = 1;
+    }
+    else if(TYPE==2){
+        dst = "/root/data/fufa/modelcompression/quantization/checkpoints/wafer-train-int8.trt";
+        calibrator_dataset_path = "/root/data/fufa/modelcompression/Datasets/wafer/data/val/";
+        imageSuffix = ".bmp";
+        calibrationNum = 1000;
+        mode = 2;
+    }
 
     // builder
     auto gLogger = std::make_unique<NVLogger>(nvinfer1::ILogger::Severity::kINFO);
@@ -67,7 +183,10 @@ int main(int argc, char** argv){
         config->setFlag(BuilderFlag::kFP16);
     }
     else if(mode == 2){
-        std::cout<<"not supported!"<<std::endl;
+            config->setFlag(BuilderFlag::kINT8);
+            pCalibrator = new CNNCalibrator(calibrator_dataset_path, imageSuffix, calibrationNum);
+            config->setInt8Calibrator(pCalibrator);
+            config->setDLACore(config->getDLACore());
     }
 
     // plan
